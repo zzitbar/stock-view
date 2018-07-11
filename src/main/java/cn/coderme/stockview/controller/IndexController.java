5d6b43313e7a86197349ccc14e93ec8177850b9a
greenman0007 <greenman0007@gmail.com>
2018-07-11 18:09:51 +0800

    首页修改

--- src/main/java/cn/coderme/stockview/controller/IndexController.java
+++ src/main/java/cn/coderme/stockview/controller/IndexController.java
@@ -1,11 +1,17 @@
 package cn.coderme.stockview.controller;
 
+import cn.coderme.stockview.entity.LiveRoom;
+import cn.coderme.stockview.service.LiveRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 /**
  * Created By Administrator
@@ -13,15 +19,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Time:17:35
  */
 @Controller
-@RequestMapping(value = {"/index", ""})
+@RequestMapping()
 public class IndexController {
 
     @Autowired(required = false)
     private ThreadPoolTaskExecutor taskExecutor;
 
+    @Autowired
+    private LiveRoomService liveRoomService;
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model) {
+        LiveRoom liveRoom = liveRoomService.findByLiveDate(LocalDate.now());
+        model.addAttribute("room", liveRoom);
         return "index";
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String main(Model model) {
+        LiveRoom liveRoom = liveRoomService.findByLiveDate(LocalDate.now());
+        model.addAttribute("room", liveRoom);
+        return "main";
+    }
+
+    @RequestMapping(value = "/stock", method = RequestMethod.GET)
+    public String stock() {
+        return "stockIndex";
     }
 
     @RequestMapping(value = "/taskActiveCount")
